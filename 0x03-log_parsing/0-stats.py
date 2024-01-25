41bb1d4885d5180def1a2f1231d41b635847d6f9
odhiambow2354 <odhiambow2354@gmail.com>
2024-01-25 08:28:30 +0300

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
 
 import sys
 import re
@@ -9,42 +6,43 @@ import re
 
 def output(log: dict) -> None:
     """
-    a helper function to display stats
+    Helper function to display stats
     """
-    print("File size: {}".format(log["file_size"]))
+    print("Total file size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+        count = log["code_frequency"][code]
+        if count:
+            print("{}: {}".format(code, count))
 
 
 if __name__ == "__main__":
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
 
     line_count = 0
     log = {}
     log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+    log["code_frequency"] = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-            if (match):
+            if match:
                 line_count += 1
-                code = match.group(1)
+                code = int(match.group(1))  # Convert code to integer
                 file_size = int(match.group(2))
 
                 # File size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
+                # Status code
+                if code in log["code_frequency"]:
                     log["code_frequency"][code] += 1
 
-                if (line_count % 10 == 0):
+                if line_count % 10 == 0:
                     output(log)
+    except KeyboardInterrupt:
+        pass
     finally:
         output(log)
